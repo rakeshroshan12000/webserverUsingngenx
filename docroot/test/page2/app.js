6a83ac75e7e5cdf163633e6ac93438aa63fd89d8
rakeshroshan12000 <rakesh_roshan12000@yahoo.com>
2020-04-26 02:11:18 +0530

    2 page navigation
    2 page navigation

---
+++ docroot/test/page2/app.js
@@ -0,0 +1,14 @@
+'use strict'
+function msg(){  
+    var x =    document.forms["myForm"]["userId"].value;
+    if(x != '11') {
+        alert("input wrong : -" + x);
+        return false;
+    }
+//     alert("Hello Javatpoint");  
+//     console.log('Here\'s a hidden message');
+// let today = new Date();
+// let formatDate = today.toDateString();
+// let selectElement = document.getElementById('date');
+// selectElement.innerHTML = formatDate;
+   }  
